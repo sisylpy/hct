5d334c7c72060b0047c98891ca766a8ef4849374
lipeiyi <454926763@qq.com>
2018-07-15 15:56:39 +0800

    Swolo12345!@#$%

--- models/ConnPool.js
+++ models/ConnPool.js
@@ -5,7 +5,7 @@ module.exports = (function () {
     var pool = mysql.createPool({
         host: 'localhost',       //主机
         user: 'root',               //MySQL认证用户名
-        password: '123456',        //MySQL认证用户密码
+        password: 'Swolo12345!@#$%',        //MySQL认证用户密码
         database: 'segment',
         port: '3306',                   //端口号
         dateString: true
